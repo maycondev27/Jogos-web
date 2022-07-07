c602dd34b0599213bfe5bdba2484ddb2123f2dde
maycondev27 <bmaycondev1@gmail.com>
2022-07-07 10:32:58 -0300

    att

---
+++ anotacoes.js
@@ -0,0 +1,33 @@
+// overflow: hidden;  //CSS3, comando que faz que tudo que sair da tela, não apareça mais
+//--------------------------------------------
+
+// keyframes // Cada keyframe descreve como o elemento animado deveria se renderizar a um tempo dado durante a sequência de animação. Como a sincronização da animação é definida por um estilo CSS que configura a animação, keyframes usam uma percentage (en-US) para indicar o tempo durante a sequência de animação que eles fazem parte.
+
+//--------------------------------------------
+
+// animations //Animações CSS tornam possível animar transições de um estilo CSS para outro. Animações se consistem de dois componentes: um estilo descrevendo a animação e um set de keyframes que indicam o estado final e inicial do estilo CSS da animação, bem como possíveis waypoints intermediários ao longo do caminho.
+
+//--------------------------------------------
+//  animation: pipe-animations 2s infinite linear;
+//  2sg é velocidade da animação na tela, 
+//infinite é para ele ficar se repetindo em loop e 
+//linear para ele ficar na mesma velocidade
+
+//--------------------------------------------
+
+/* @keyframes pipe-animations {
+from {
+right: -80px;
+-80px a direta foi para ele entrar de fora da tela e não surgir do nada
+
+//--------------------------------------------
+
+
+bottom: 0; no .mario -> foi para ele ficar colado certinho no chão do quadro 
+
+document.addEventListener("keydown", jump); -> quando acionar uma tecla do teclado vai acionar uma função
+jump foi a função acionada.
+
+const mario = document.querySelector(".mario") -> selecionando a imagem do mario pela classe ''.''
+
+*/
